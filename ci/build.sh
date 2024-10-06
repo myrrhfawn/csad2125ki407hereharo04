11342a4246764d2d5a3d24d539ab82ddd1a77c52
myrrhfawn <rostyk2003@gmail.com>
2024-10-07 01:26:35 +0300

    change absolute path

--- ci/build.sh
+++ ci/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -n "$GITHUB_WORKSPACE" ]; then
-  PROJECT_DIR = $$GITHUB_WORKSPACE
+  PROJECT_DIR=$$GITHUB_WORKSPACE
 else
   PROJECT_DIR="/data/APKS/csad2125ki407hereharo04/"  # Binary file name
 fi
 
 BOARD="arduino:avr:nano"       # Board name
-SKETCH_NAME="$GITHUB_WORKSPACE/COMServer/COMServer.ino"  # Binary file name
+SKETCH_NAME="$PROJECT_DIR/COMServer/COMServer.ino"  # Binary file name
 export PATH=$PATH:$PWD/bin/
 
 sudo apt-get install tree
@@ -25,4 +25,4 @@ arduino-cli core update-index
 
 # Compile sketch
 echo "Compile $SKETCH_NAME"
-arduino-cli compile --fqbn $BOARD --output-dir $PROJECT_DIR/ci/build --verbose $SKETCH_NAME
+arduino-cli compile --fqbn $BOARD --output-dir "$PROJECT_DIR/ci/build" $SKETCH_NAME
